b74ff7aea855ed376142e347bf1a6b54b157a0ff
harshita-jain28 <harshita282002@gmail.com>
2022-06-25 22:38:09 +0530

    project setup

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var dotenv = require("dotenv");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("chal gya ka badla");
+});
+app.post("/", (req, res) => {
+  var name = req.body.email;
+  var pass = req.body.password;
+  console.log(name, pass);
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`chal gya at ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+function App() {
+  const [username, setUsername] = useState();
+  const [pass, setPassword] = useState();
+
+  const submitForm = () => {
+    console.log(username, pass);
+    fetch("http://localhost:8080/", {
+      method: "POST",
+      body: JSON.stringify({ email: username, password: pass }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => console.log(data));
+  };
+  return (
+    <div className="App">
+      <form>
+        <label>Email</label>
+        <input
+          type="email"
+          name="Email"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={submitForm}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
