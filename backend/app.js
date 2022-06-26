b11d279aea845db1f031afa458930851b7ee5cc5
Srejan Kumar Bera <srejan.bera@gmail.com>
2022-06-26 22:42:14 +0530

    Backend Updated,Database config added.

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,21 @@ var dotenv = require("dotenv");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
+const connectDB = require('./config/db_connect')
 dotenv.config();
+connectDB();
+
+const User = require('./models/userData')
+const userRouter = require('./routes/userRouter')
+
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use(express.json());
+app.use('/users',userRouter)
+
 
 app.get("/", (req, res) => {
   res.send("chal gya ka badla");
