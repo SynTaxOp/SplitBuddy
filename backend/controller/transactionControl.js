22b3ebe0e7c0c986b738b0b1ee08fa20b32448ad
Srejan Kumar Bera <srejan.bera@gmail.com>
2022-07-11 23:58:41 +0530

    TransactionControl fixed,almost completed

--- backend/controller/transactionControl.js
+++ backend/controller/transactionControl.js
@@ -1,55 +1,87 @@
 const User = require("../models/userData");
+const Splitwise = require("splitwise-js-map");
 
 const addTransaction = async (req, res) => {
-  const { username, group_name, payer_name ,amount,payees } = req.body;
+  const { username, group_name, payer_name, amount, payees } = req.body;
 
-  var amt = amount / payees.length
-  console.log('Amount',amt);
+  var amt = amount / payees.length;
+  console.log("Amount", amt);
   const user = await User.findOne({ username });
+  var group_idx = 0;
+  for (let index = 0; index < user.Groups.length; index++) {
+    if (user.Groups[index].group_name == group_name) {
+      group_idx = index;
+      break;
+    }
+  }
+  console.log("GROUP_IDX:", group_idx);
+  var Groups_array = user.Groups;
+  var group_data = user.Groups[group_idx];
+  var prev_transaction_data = user.Groups[group_idx].transaction_data;
+  var transaction_index = -1;
+  for (let i = 0; i < prev_transaction_data.length; i++) {
+    if (prev_transaction_data[i].paidBy == payer_name) {
+      transaction_index = i;
+      break;
+    }
+  }
+
+  if (transaction_index == -1) {
+    var new_transaction = { paidBy: payer_name, paidFor: {} };
+    payees.forEach((element) => {
+      new_transaction.paidFor[element] = amt;
+    });
+    prev_transaction_data.push(new_transaction);
+  } else {
+    var new_transaction = prev_transaction_data[transaction_index];
+    payees.forEach((element) => {
+      if (new_transaction.paidFor[element] == undefined) {
+        new_transaction.paidFor[element] = 0;
+      }
+      new_transaction.paidFor[element] += amt;
+    });
+    prev_transaction_data[transaction_index] = new_transaction;
+  }
+  group_data.transaction_data = prev_transaction_data;
+  Groups_array[group_idx] = group_data;
+  User.updateOne(
+    { username },
+    {
+      Groups: Groups_array,
+    },
+    (err) => {
+      if (err == null) {
+        res.status(200).json({ msg: "Update Success" });
+      }
+      else
+      {
+        res.status(400).json({ msg:err})
+      }
+    }
+  );
+};
+
+const generateSplitwise = async (req, res) => 
+{
+  const username = req.query.username;
+  const group_name = req.query.title;
   const group = await User.findOne(
     { username },
-    { Groups: { $elemMatch: { group_name } } }, 
+    { Groups: { $elemMatch: { group_name } } },
     (err) => {
       if (err) {
         console.log(err);
       }
     }
   ).clone();
-  
-  User.updateOne({ username, Groups: { $elemMatch: { group_name } } },
-    {
-        Groups$transaction_data :  
-    } 
-
-  if (group) {
-    var prev_transaction_data = group.Groups[0].transaction_data;
-    console.log(prev_transaction_data)
-
-    var found_payer_data = prev_transaction_data.filter(element=>element.paidBy==payer_name)[0];
-    if(found_payer_data==undefined)
-    {
-      found_payer_data = {paidBy:payer_name,paidFor:{}};
-    }
-    payees.forEach(element => {
-        if(found_payer_data.paidFor[element] == undefined)
-        {
-          found_payer_data.paidFor[element] = amt; 
-        }
-        else
-        {
-          found_payer_data.paidFor[element]+=amt;
-        }
-    });
-
-    res.status(200).json({
-      members: prev_transaction_data,
-    });
-  } else {
-    console.log('Badh me jaao')
-    res.status(400);
-    throw new Error("Error");
-  }
-
-};
-
-module.exports = { addTransaction};
+  var found_transaction_data = group.Groups[0].transaction_data;
+  var splitwise_data = found_transaction_data.map((ele) => ({
+  paidBy: ele.paidBy,
+  paidFor: ele.paidFor,
+}));
+  console.log(splitwise_data);
+  const splits = Splitwise(splitwise_data);
+  console.log(splits);
+  res.send(splits);
+}
+module.exports = { addTransaction,generateSplitwise };
--- backend/routes/transactionRouter.js
+++ backend/routes/transactionRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {
-  addTransaction
+  addTransaction,generateSplitwise
 } = require("../controller/transactionControl");
 
 router.route("/addTransaction").post(addTransaction);
-
+router.route("/generateSplitwise").get(generateSplitwise);
 module.exports = router;
